c799f3653038094b7640664b2ea946fc65bffc3f
Nirmit Agrawal <38294311+NirmitAgrawal02@users.noreply.github.com>
2022-10-15 18:02:58 -0700

    Add files via upload

---
+++ Facade.java
@@ -0,0 +1,62 @@
+public class Facade {
+
+    private int UserType;
+
+    private Product  theSelectProduct;
+
+    private int nProductCategory;
+
+    private ClassProductList theProductList;
+
+    private Person thePerson;
+
+    public boolean login()
+    {
+
+    }
+    public void addTrading()
+    {
+
+    }
+    public void viewTrading()
+    {
+
+    }
+    public void viewOffering()
+    {
+
+    }
+    public void markOffering()
+    {
+
+    }
+    public void submitOffering()
+    {
+
+    }
+    public void remind()
+    {
+
+    }
+    public void createUser(UserInformation userinfoitem)
+    {
+
+    }
+    public void createProductList()
+    {
+
+    }
+    public void attachProductToUser()
+    {
+
+    }
+    public void selectProduct()
+    {
+
+    }
+    public void productOperation()
+    {
+
+    }
+
+}
