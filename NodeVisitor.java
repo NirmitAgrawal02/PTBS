8824daf48f49372bf007e79d77c68c7e52358f34
Nirmit Agrawal <38294311+NirmitAgrawal02@users.noreply.github.com>
2022-10-17 18:56:49 -0700

    Node Visitor V1.0

--- NodeVisitor.java
+++ NodeVisitor.java
@@ -1,15 +1,18 @@
-public class NodeVisitor {
+/**
+ * Node Visitor class for Visitor Pattern
+ * @author Nirmit Agrawal, nagraw18@asu.edu
+ * @version 1.0
+ * @since 10-17-2022
+ */
 
-	public void visitProduct(Product product) {
 
-	}
 
-	public void visitTrading(Trading trading) {
+public abstract class NodeVisitor {
 
-	}
+	public abstract void visitProduct(Product product);
 
-	public void visitFacade(Facade facade) {
+	public abstract void visitTrading(Trading trading);
 
-	}
+	public abstract void visitFacade(Facade facade);
 
 }
