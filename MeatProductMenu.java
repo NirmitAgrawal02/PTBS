d0a38b752174a8dc46245dc251d1da486bbda211
Nirmit Agrawal <38294311+NirmitAgrawal02@users.noreply.github.com>
2022-10-17 17:20:27 -0700

    Meat Product Menu V1.0

--- MeatProductMenu.java
+++ MeatProductMenu.java
@@ -1,27 +1,55 @@
+/**
+ * SER-515 Meat Product Menu responsible for bridge and factory patterns
+ *
+ * @author Nirmit Agrawal, nagraw18@asu.edu
+ * @version 1.0
+ * @since 10-17-2022
+ */
+
 public class MeatProductMenu implements ProductMenu {
 
 	public void showMenu() {
 
 	}
 
+	@Override
 	public void showAddButton() {
+		System.out.println();
+	}
+	@Override
+	public void showViewButton() {
+		System.out.println();
 
 	}
-
-	public void showvViewButton() {
-
-	}
-
+	@Override
 	public void showRadioButton() {
+		System.out.println();
 
 	}
-
+	@Override
 	public void showLabels() {
+		System.out.println();
 
 	}
-
+	@Override
 	public void showComboxes() {
+		System.out.println();
 
+	}
+	public void selectProduct(int UserType)
+	{
+		System.out.println("Bridge Pattern for connection used");
+		System.out.println("Meat Products Selected ....");
+		Person person;
+		if(UserType == 0)
+		{
+			person = personFactory.createObject("Buyer");
+		}
+		else
+		{
+			person = PersonFactory.createObject("Seller");
+		}
+		person.showMenu();
 	}
 
 }
