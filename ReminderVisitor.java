37492c4e71e9970e67cd8d1df5f34e3093718333
Nirmit Agrawal <38294311+NirmitAgrawal02@users.noreply.github.com>
2022-10-17 18:57:22 -0700

    Reminder Visitor V1.0

--- ReminderVisitor.java
+++ ReminderVisitor.java
@@ -1,19 +1,27 @@
+
+
+/**
+ * Abstract ReminderVisitor Class for Visitor Pattern
+ * @author Nirmit Agrawal, nagraw18@asu.edu
+ * @version 1.0
+ * @since 10-17-2022
+ */
 public class ReminderVisitor extends NodeVisitor {
 
 	private Reminder m_Reminder;
 
-	private ClassProductList classProductList;
-
+	private ProductList productList;
+	@Override
 	public void visitProduct(Product product) {
-
+		System.out.println("Visiting Product");
 	}
-
+	@Override
 	public void visitTrading(Trading trading) {
-
+		System.out.println("Visiting Trading");
 	}
-
+	@Override
 	public void visitFacade(Facade facade) {
-
+		System.out.println("Visiting Facade");
 	}
 
 }
