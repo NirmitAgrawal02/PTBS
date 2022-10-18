000fd45e965f3d79d4295b5be99a2fb23c573c85
Nirmit Agrawal <38294311+NirmitAgrawal02@users.noreply.github.com>
2022-10-17 17:45:22 -0700

    Reminder V1.0

--- Reminder.java
+++ Reminder.java
@@ -1,3 +1,12 @@
-public class Reminder {
+
+/**
+ * Abstract Reminder Class for Visitor Pattern
+ * @author Nirmit Agrawal, nagraw18@asu.edu
+ * @version 1.0
+ * @since 10-17-2022
+ */
+public abstract class Reminder {
+
+    public abstract Reminder accept(NodeVisitor visitor);
 
 }
